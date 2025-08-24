641108e0581af043c6f0bbef164a710494a295cb
Lalitforreal <s24cseu0093@bennett.edu.in>
2025-08-24 15:18:18 +0530

    Daily progress

---
+++ pp.js
@@ -0,0 +1,6 @@
+        var typed = new Typed('#element', {
+        strings: ['Sanmica Manufacturing,', 'Production line management,', 'and Quality Control.'],
+        typeSpeed: 40,    // Faster typing (40ms per character)
+        backSpeed: 20,    // Optional, faster deleting
+        loop: true  
+        });
