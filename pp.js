cf4f39a1e41d05d5d2869f22d2428f87d82e568d
Lalitforreal <s24cseu0093@bennett.edu.in>
2025-08-28 20:20:59 +0530

    Final draft for hero section media query/ animations left

--- pp.js
+++ pp.js
@@ -6,7 +6,7 @@ const scroll = new LocomotiveScroll({
 });
 
 var typed = new Typed('#element', {
-strings: ['Sanmica Manufacturing,', 'Production line management,', 'and Quality Control.'],
+strings: ['Sanmica Manufacturing,', 'Production Management,', 'and Quality Control.'],
 typeSpeed: 40,    // Faster typing (40ms per character)
 backSpeed: 20,    // Optional, faster deleting
 loop: true  
